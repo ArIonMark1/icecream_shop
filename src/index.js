36c193e86c0676e390ca8cf80dee156ee573e1ca
ArIonMark1 <andreydatsenko777@gmail.com>
2023-02-19 22:23:07 +0100

    team_project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+import './js/about-modal';
+import './js/franchise-modal.js';
+import './js/locations-modal.js';
+import './js/products-modal-1.js';
+import './js/products-modal-2.js';
+import './js/products-modal-3.js';
+import './js/franchise-modal.js';
+import './js/header-mobile.js';
+import './js/reviews-slider';
+import './js/active_window';
